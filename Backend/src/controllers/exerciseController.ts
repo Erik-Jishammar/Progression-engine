28ac800529ccb373ba7e2230eb2796537004770f
Erik Jishammar <erikjishammar@hotmail.com>
2026-05-08 09:16:15 +0200

    feat: add exerciseController (getAllExercises,  getExerciseById)

--- Backend/src/controllers/exerciseController.ts
+++ Backend/src/controllers/exerciseController.ts
@@ -18,3 +18,48 @@ export const createExercise = async (req: Request, res: Response) => {
         
     }
 }
+export const getAllExercises = async (req:Request, res:Response) => {
+    try{
+        const allExercises = await prisma.exercise.findMany({
+            include:{
+                setEntries: true,
+            },
+            orderBy:{
+                name: 'asc'
+            }
+        })
+        res.status(200).json(allExercises)
+    } catch (error) {
+        console.error("error fetching exercises",error)
+        res.status(500).json({error: "failed to fetch exercises"})
+    }
+}   
+export const getExerciseById = async (req:Request, res:Response) => {
+    try {
+        const {id} = req.params; 
+
+        const exercise = await prisma.exercise.findUnique({
+            where: {
+                id: Number(id)
+            },
+            include: {
+                setEntries: {
+                    include: {
+                        workoutSession:true
+                    },
+                    orderBy: {
+                        createdAt: 'desc' // latest first
+                    }
+                }
+            }
+        })
+        if (!exercise) {
+            return res.status(404).json({error: "exercise not found"})
+        }
+        res.status(200).json(exercise)
+
+    } catch (error){
+    console.error("error fetching exercise by ID", error); 
+    res.status(500).json({error: "failed to fetch  specific exercise"})
+    }
+}
